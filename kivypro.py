6a4f49636bbb7eedaedc1c18894e49bdaa2bfd92
Olezha <oleg.bushilo.00@gmail.com>
2019-10-20 13:37:32 +0300

    second commit

--- kivypro.py
+++ kivypro.py
@@ -1,56 +1,52 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.config import Config
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.graphics import *
+from kivy.uix.floatlayout import FloatLayout
+import time
+
+class StartScreen(Screen):
+	def build(self):
+		#fl = FloatLayout(size = (360,640))	
+		
+		#return fl
+		pass
+class LoadScreen(Screen):
+	pass
+class RegScreen(Screen):
+	pass
+class AdminPage(Screen):
+	pass
+class ClubPage(Screen):
+	pass
+class UserPage(Screen):
+	pass
+class News(ScreenManager):                     #Root screen
+	pass
+class Clubs(Screen):
+	pass
+class Chats(Screen):
+	pass
+class Users(Screen):
+	pass
+class Table(Screen):
+	pass
 
 Config.set("graphics","resizable","0")
 Config.set("graphics","width","360")
 Config.set("graphics","height", "640")
 
 from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.scatter import Scatter
 
 class MyApp(App):
 	def build(self):
-		#s = Scatter()
-		fl = FloatLayout(size = (300,300))
-		#s.add_widget(fl)
-		fl.add_widget(Button(text="пшол нах", 
-			font_size = 30, 
-			on_press = self.btn_press,
-			background_color = [1,0,0,1],
-			background_normal = '',
-			size_hint = (.5, .25),
-			pos = (160, 180)))
-		
-		return fl
-	
-	def btn_press(self, instance):
-		print ("сам иди")
-		instance.text = 'ацтань'
-
+		with self.canvas:
+			Color(1.,0,0)
+			Rectangle(pos = (0,0), size = (360,640))
+		#return StartScreen()
+		"""time.sleep(4)
+								return News()"""
 if __name__ == "__main__":
 	MyApp().run()
-
-
-
-
-
-"""
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.config import Config
-
-Config.set("graphics","resizable","0")
-Config.set("graphics","width","640")
-Config.set("graphics","height", "480")
-
-class MyApp(App):
-	def build(self):
-		bl = BoxLayout(orientation = "vertical")
-		bl.add_widget(Button(text = "first button"))
-		bl.add_widget(Button(text = "the second one"))
-		bl.add_widget(Button(text = "the third"))
-		return bl
-if __name__ == "__main__":
-	MyApp().run()"""
+	#полная хуита
